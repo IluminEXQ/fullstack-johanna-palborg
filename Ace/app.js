ef1fdd6680096322a11eb43fdeb75240c59f73dc
IluminEXQ <70211741+IluminEXQ@users.noreply.github.com>
2020-11-04 16:22:20 +0100

    ejs

--- Ace/app.js
+++ Ace/app.js
@@ -3,24 +3,24 @@ const databaseModule = require('./databasemodule.js')
 const app = express()
 const port = 1337
 
-app.use(express.json())
-app.use(express.urlencoded())
-
 const clientDir = __dirname + "\\client\\"
 
-app.get('/', (req, res) => res.sendFile(clientDir + 'index.html'))
-app.get('/ok', (req, res) => {
-  res.sendFile(clientDir + 'main_style.css')
-})
-app.get('/sad', (req, res) => {
-  res.sendFile(clientDir + 'Cat.png')
-})
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(express.static(clientDir))
 
-app.post('/sendMessage', function (req, res) {
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', {name : "Ilumin"})
+})
+  
+  app.post('/sendMessage', function (req, res) {
     console.log(req.body.name)
     console.log(req.body.message)
     
    databaseModule.saveMessage(req.body.name, req.body.message)
+   
 
     res.redirect('/success')
 })
